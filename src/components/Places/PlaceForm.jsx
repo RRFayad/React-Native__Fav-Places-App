f66a603e4d5ea2c165e745a1f0f01e3ff36fa422
RRFayad <renan.r.fayad@gmail.com>
2024-02-27 20:08:36 -0300

    feat: Started Add New Place Form

--- src/components/Places/PlaceForm.jsx
+++ src/components/Places/PlaceForm.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, TextInput } from "react-native";
 
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
   return (
-    <View>
-      <Text>Place Form</Text>
-    </View>
+    <ScrollView className="flex-1 p-6">
+      <View>
+        <Text className="mb-1 font-bold text-primary-500">TITLE</Text>
+        <TextInput
+          className="my-2 border-2 border-b-primary-700 bg-primary-100 px-1 py-2 text-base"
+          onChangeText={(enteredText) => setEnteredTitle(enteredText)}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
